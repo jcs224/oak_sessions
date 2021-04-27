d7442dac359036b59889f1a90ff2ad9532ba6d4a
Joe Sweeney <joe.sweeney224@gmail.com>
2021-04-26 18:35:09 -0600

    add opine back in as js

--- mod.ts
+++ mod.ts
@@ -1,4 +1,5 @@
 import OakSession from './src/frameworks/OakSession.js'
+import OpineSession from './src/frameworks/OpineSession.js'
 import MemoryStore from './src/stores/MemoryStore.js'
 import SqliteStore from './src/stores/SqliteStore.js'
 import RedisStore from './src/stores/RedisStore.js'
@@ -6,6 +7,7 @@ import WebdisStore from './src/stores/WebdisStore.js'
 
 export {
   OakSession,
+  OpineSession,
   MemoryStore,
   SqliteStore,
   RedisStore,
---
+++ src/frameworks/OpineSession.js
@@ -0,0 +1,32 @@
+import Session from '../Session.js'
+import { getCookies } from "https://deno.land/std@0.93.0/http/cookie.ts";
+
+export default class OpineSession extends Session {
+  constructor(opineApp, options = {}, store) {
+    super(store || null)
+
+    opineApp.use(async (req, res, next) => {
+      const { sid } = getCookies(req);
+
+      if (req.url == '/favicon.ico') {
+        await next()
+      }
+
+      if (!options.secure) {
+        options.secure = req.protocol === "https" ? true : false;
+      }
+      if (!options.path) {
+        options.path = "/";
+      }
+
+      if (sid && await this.sessionExists(sid)) {
+        req.session = this.getSession(sid)
+      } else {
+        req.session = await this.createSession()
+        res.cookie('sid', req.session.id)
+      }
+
+      await next()
+    })
+  }
+}
