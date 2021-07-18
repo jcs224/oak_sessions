d1e60d18d6bb83d531cdc3e49e97f99857761745
Joe Sweeney <joe.sweeney224@gmail.com>
2021-07-17 22:40:21 -0600

    upgrade form param parser

--- src/Helpers.js
+++ src/Helpers.js
@@ -1,12 +1,34 @@
 export async function parseFormParams(ctx) {
   const params = new Map
-  
-  if (await ctx.request.body().value) {
-    const value = await ctx.request.body().value
 
-    value.forEach((value, key) => {
-      params.set(key, value)
-    })
+  if (ctx.request.hasBody) {
+
+    const requestBody = ctx.request.body()
+
+    switch(requestBody.type) {
+      case 'json':
+        const jsonPayload = await requestBody.value
+
+        for (const prop in jsonPayload) {
+          params.set(prop, jsonPayload[prop])
+        }
+        break;
+      case 'form':
+        const formPayload = await requestBody.value
+        formPayload.forEach((value, key) => {
+          params.set(key, value)
+        })
+        break;
+      case 'form-data':
+        const formDataPayload = requestBody.value
+        const formData = await formDataPayload.read()
+        const fields = formData.fields
+        console.log(fields)
+
+        for (const prop in fields) {
+          params.set(prop, fields[prop])
+        }
+    }
   }
 
   return params
