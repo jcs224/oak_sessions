9f7e553bc23f0b09b42ecbefb3b74db6a3f4ae7b
Joe Sweeney <joe.sweeney224@gmail.com>
2021-08-29 04:52:15 -0600

    clean up redis cruft

--- src/stores/RedisStore.js
+++ src/stores/RedisStore.js
@@ -1,16 +1,7 @@
-import { connect } from 'https://deno.land/x/redis@v0.22.2/mod.ts'
-
 export default class RedisStore {
   constructor(db, keyPrefix = 'session_') {
     this.keyPrefix = keyPrefix
     this.db = db
-  }
-
-  async init() {
-    this.db = await connect({
-      hostname: this.host,
-      port: this.port
-    })
   }
 
   async sessionExists(sessionId) {
