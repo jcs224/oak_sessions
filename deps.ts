728107f725dfc7f3121819b9e05d01e54ecdefed
Joe Sweeney <joe.sweeney224@gmail.com>
2022-04-21 00:02:44 -0600

    update oak to 10.5.1

--- deps.ts
+++ deps.ts
@@ -1,13 +1,13 @@
 export {
   Application,
   Router
-} from 'https://deno.land/x/oak@v10.5.0/mod.ts'
+} from 'https://deno.land/x/oak@v10.5.1/mod.ts'
 
 export {
   Context
-} from 'https://deno.land/x/oak@v10.5.0/context.ts'
+} from 'https://deno.land/x/oak@v10.5.1/context.ts'
 
 export type {
   CookiesGetOptions, 
   CookiesSetDeleteOptions
-} from 'https://deno.land/x/oak@v10.5.0/cookies.ts'
+} from 'https://deno.land/x/oak@v10.5.1/cookies.ts'
