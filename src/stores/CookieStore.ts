3dc919122d5755343b0b87889768c6e8367e3dc6
c0per <60544162+c0per@users.noreply.github.com>
2022-07-20 10:11:37 +0800

    refactor: Session and CookieStore
    `Session` instance now stands for one request-response session.
    
    `CookieStore` instance now stands for a store for all the session data
    from different ctx.
    
    `CookieStore` doesn't implement `Store` anymore.

--- src/stores/CookieStore.ts
+++ src/stores/CookieStore.ts
@@ -1,91 +1,67 @@
 import CryptoJS from 'https://cdn.skypack.dev/crypto-js@4.1.1'
-import Store from './Store.ts'
 import type { Context, CookiesGetOptions, CookiesSetDeleteOptions } from '../../deps.ts'
-import { SessionData } from '../Session.ts'
+import type { SessionData } from '../Session.ts'
 
 interface CookieStoreOptions {
   cookieGetOptions?: CookiesGetOptions;
   cookieSetDeleteOptions?: CookiesSetDeleteOptions;
 }
 
-export default class CookieStore implements Store{
-  data: SessionData | null
-  
+export default class CookieStore {
   encryptionKey: string | null
-  context : Context | null
 
   cookieGetOptions: CookiesGetOptions;
   cookieSetDeleteOptions: CookiesSetDeleteOptions;
 
   constructor(encryptionKey : string | null = null, options? : CookieStoreOptions) {
-    this.data = null
     this.encryptionKey = encryptionKey
-    this.context = null
 
     this.cookieGetOptions = options?.cookieGetOptions ?? {}
     this.cookieSetDeleteOptions = options?.cookieSetDeleteOptions ?? {}
   }
 
-  async insertSessionMiddlewareContext(ctx : Context) {
-    this.context = ctx
+  async getSessionByCtx(ctx : Context) : Promise<SessionData | null> {
+    const sessionDataString : string | undefined = await ctx.cookies.get('session_data', this.cookieGetOptions)
 
-    const sessionDataString : string | undefined = await this.context.cookies.get('session_data', this.cookieGetOptions)
+    if (!sessionDataString) return null;
 
-    if (await this.sessionExists()) {
-      if (this.encryptionKey) {
-        const rawString = sessionDataString
-        let bytes = CryptoJS.AES.decrypt(rawString, this.encryptionKey)
-        let decryptedCookie : string = ''
+    if (this.encryptionKey) {
+      const rawString = sessionDataString
+      const bytes = CryptoJS.AES.decrypt(rawString, this.encryptionKey)
 
-        try {
-          decryptedCookie = bytes.toString(CryptoJS.enc.Utf8)
-        } catch (e) {
-          this.data = null
-        }
+      try {
+        const decryptedCookie = bytes.toString(CryptoJS.enc.Utf8)
+        return JSON.parse(decryptedCookie)
+      } catch (_e) {
+        return null
+      }
         
-        if (decryptedCookie) {
-          try {
-            this.data = JSON.parse(decryptedCookie)
-          } catch (e) {
-            this.data = null
-          }
-        }
-      } else {
-        try {
-          this.data = typeof sessionDataString == 'string' ? JSON.parse(sessionDataString) as SessionData : null
-        } catch (e) {
-          this.data = null
-        }
+    } else {
+      try {
+        return typeof sessionDataString === 'string' ? JSON.parse(sessionDataString) : null
+      } catch (_e) {
+        return null
       }
     }
   }
 
-  async sessionExists() {
-    return await this.context?.cookies.get('session_data', this.cookieGetOptions) ? true : false
+  async createSession(ctx : Context, initialData : SessionData) {
+    let dataString = JSON.stringify(initialData)
+
+    if (this.encryptionKey)
+      dataString = CryptoJS.AES.encrypt(dataString, this.encryptionKey).toString()
+    await ctx.cookies.set('session_data', dataString, this.cookieSetDeleteOptions)
   }
 
-  async getSessionById() {
-    return await this.context?.cookies.get('session_data', this.cookieGetOptions) ? this.data : null
+  deleteSession(ctx : Context) {
+    ctx.cookies.delete('session_data', this.cookieSetDeleteOptions)
   }
 
-  createSession(id : string, initialData : SessionData) {
-    this.data = initialData
-  }
+  async persistSessionData(ctx : Context, data : SessionData) {
+    let dataString = JSON.stringify(data)
 
-  async deleteSession(ctx : Context) {
-    await this.context?.cookies.delete('session_data', this.cookieSetDeleteOptions)
-  }
-
-  persistSessionData(id : string, sessionData : SessionData) {
-    this.data = sessionData
-  }
-
-  async afterMiddlewareHook() {
-    if (this.encryptionKey) {
-      let cipherText = CryptoJS.AES.encrypt(JSON.stringify(this.data), this.encryptionKey).toString()
-      await this.context?.cookies.set('session_data', cipherText, this.cookieSetDeleteOptions)
-    } else {
-      await this.context?.cookies.set('session_data', JSON.stringify(this.data), this.cookieSetDeleteOptions)
-    }
+    if (this.encryptionKey)
+      dataString = CryptoJS.AES.encrypt(dataString, this.encryptionKey).toString()
+    await ctx.cookies.set('session_data', dataString, this.cookieSetDeleteOptions)
   }
 }
